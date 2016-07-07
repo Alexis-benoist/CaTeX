f8f128f63fd54b746f492ed086590259e8cb4453
Alexis Benoist <alexis.benoist@gmail.com>
2016-07-07 22:21:03 +0200

    make_release: rm readme conversion.

--- script/make_release.py
+++ script/make_release.py
@@ -26,8 +26,6 @@ def build_and_upload():
     rm('CaTeX.egg-info')
     rm('build')
     rm('dist')
-    Popen(['pandoc', '--from=markdown', '--to=rst', 'readme.md', '--output=readme.rst'],
-           stdout=PIPE).wait()
     Popen([sys.executable, 'setup.py', 'bdist_wheel', '--universal'], stdout=PIPE).wait()
     Popen([sys.executable, 'setup.py', 'sdist'], stdout=PIPE).wait()
     pypi_pwd = getpass(prompt='Pypi Password: ')
